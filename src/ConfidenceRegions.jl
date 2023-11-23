3dccba52286bc66d228e01e818e3a940e5c79ba9
Rafael Arutjunjan <arutjunjan.r@gmail.com>
2023-11-23 15:51:41 +0100

    Update compat

--- src/ConfidenceRegions.jl
+++ src/ConfidenceRegions.jl
@@ -502,7 +502,7 @@ function StructurallyIdentifiable(DM::AbstractDataModel, mle::AbstractVector{<:N
     J = reduce(vcat, [EmbeddingMatrix(DM, mle + noise .* (rand(length(mle)) .- 0.5)) for i in 1:N])
     _, S, Vt = svd(J)
     nonids = count(x->x<thresh, S)
-    nonids == 0 && println("$(length(name(DM)) > 0 ? name(DM) : "DataModel") is structurally identifiable! Smallest singular value $(round(S[end]; sigdigits=5)) > $thresh.")
+    nonids == 0 && println("$(length(name(DM)) > 0 ? name(DM) : "DataModel") is locally structurally identifiable! Smallest singular value $(round(S[end]; sigdigits=5)) > $thresh.")
     !showall && return
     for ind in length(S):-1:1
         if showall || S[ind] < thresh
