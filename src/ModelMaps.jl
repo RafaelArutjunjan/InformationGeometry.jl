4446640e6bcb9d3469fad97e289683b1c9ee77b5
Rafael Arutjunjan <32429261+RafaelArutjunjan@users.noreply.github.com>
2026-04-23 02:24:14 +0200

    Extend Minimize() to allow for compositions of Refit, Prefit and Multistart

--- src/ModelMaps.jl
+++ src/ModelMaps.jl
@@ -44,14 +44,15 @@ end
 
 # Callback triggers when Boundaries is `true`.
 """
-    ModelMap(Map::Function, InDomain::Union{Nothing,Function}, Domain::HyperCube; startp::AbstractVector)
-    ModelMap(Map::Function, InDomain::Function, xyp::Tuple{Int,Int,Int})
+    ModelMap(Map::Function, InDomain::Union{Nothing,Function}, Domain::HyperCube; startp::AbstractVector, pnames::AbstractVector{Symbol}, inplace::Bool)
+    ModelMap(Map::Function, InDomain::Function, xyp::Tuple{Int,Int,Int}; kwargs...)
 A container which stores additional information about a model map, in particular its domain of validity.
 `Map` is the actual map `(x,θ) -> model(x,θ)`. `Domain` is a `HyperCube` which allows one to roughly specify the ranges of the various parameters.
 For more complicated boundary constraints, a function `InDomain(θ)` can be specified, for which all outputted components should be .≥ 0 on the valid parameter domain.
 Alternatively, `InDomain` may also be a bool-valued function, evaluating to `true` in admissible parts of the parameter domain.
 
-The kwarg `startp` may be used to pass a suitable parameter vector for the ModelMap.
+!!! note
+    A suitable starting parameter configuration can be passed to the ModelMap via the `startp` kwarg.
 """
 struct ModelMap{Inplace, Custom}
     Map::Function
