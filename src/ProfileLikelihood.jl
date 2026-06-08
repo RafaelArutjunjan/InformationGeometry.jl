559dc7c12bd7c58f52f659a6b95d4a4264ad10f3
Rafael Arutjunjan <32429261+RafaelArutjunjan@users.noreply.github.com>
2026-06-08 22:50:20 +0200

    Reuse VariancePropagation in ValidationProfiles
    Change kwarg structure of VariancePropagation()

--- src/ProfileLikelihood.jl
+++ src/ProfileLikelihood.jl
@@ -1263,7 +1263,7 @@ ApplyTrafoNames(S::AbstractString, F::Function; GenericName::Bool=false) = ((Tra
 
 
 # Size layout plot according to number of plots
-PlotSizer(n::Int; size::Tuple{<:Int,<:Int}=(250,250)) = (s=Int(ceil(sqrt(n)));  s .* size)
+PlotSizer(n::Int; singlesize::Int=250, size::Tuple{<:Int,<:Int}=(singlesize,singlesize)) = (s=Int(ceil(sqrt(n)));  s .* size)
 
 
 @recipe f(P::Union{ParameterProfiles, ParameterProfilesView}, S::Symbol, args...) = P, Val(S), args...
@@ -1796,7 +1796,8 @@ end
 # Ficticious point contribution accounted for via prior field, if a prior already exists then it is not saved separately in the profile object but still accounted for
 function GetValidationProfilePoint(DM::AbstractDataModel, yComp::Int, t::Union{AbstractVector{<:Number},Number}; Confnum::Real=2, N::Int=21, LogLikelihoodFn::Function=loglikelihood(DM), ModelParExtractor::Function=GetOnlyModelParams(DM),
                                 MLE::AbstractVector{<:Number}=MLE(DM), Model::ModelOrFunction=Predictor(DM), ydim::Int=InformationGeometry.ydim(DM), ypred::Real=Model(t,ModelParExtractor(MLE))[yComp], yoffset::Real=ypred,
-                                dof::Int=DOF(DM), LinPredictionUncert::Real=(C=VariancePropagation(DM, MLE; Confnum, dof)(t);   ydim>1 ? C[yComp, yComp] : C[1]),
+                                dof::Int=DOF(DM), Fisher::AbstractMatrix=FisherMetric(DM, MLE), ScaledInverseFisher::AbstractMatrix=InvChisqCDF(dof, ConfVol(Confnum)) * Symmetric(pinv(Fisher)), 
+                                VarianceProp::Function=VariancePropagation(DM, MLE, ScaledInverseFisher; Confnum, dof), LinPredictionUncert::Real=(C=VarianceProp(t);   ydim>1 ? C[yComp, yComp] : C[1]),
                                 DivideBy::Real=6, σv::Real=LinPredictionUncert/DivideBy, IC::Real=InvChisqCDF(dof, ConfVol(Confnum)), ValidationSafetyFactor::Real=2, kwargs...) # Make Confnumsafety ratio σv/(obs + σv) to decrease computations when prediction profiles are desired?
     @assert IC > 0 && dof > 0
     FicticiousPoint = Normal(0, σv)
@@ -1807,10 +1808,10 @@ function GetValidationProfilePoint(DM::AbstractDataModel, yComp::Int, t::Union{A
     VPL(θnew::ComponentVector) = LogLikelihoodFn(θnew.original) + FictDataPointPrior(θnew)
     MakeNewMLE(MLE::AbstractVector{T}) where T<:Number = [MLE; T(ypred-yoffset)]
     MakeNewMLE(MLE::ComponentVector{T}) where T<:Number = ComponentVector{T}(original=MLE; new=T(ypred-yoffset))
-    mleNew = MakeNewMLE(MLE);    Fisher = Diagonal(Fill(σv^-2,pdim(DM)+1))
+    mleNew = MakeNewMLE(MLE);    NewFisher = Diagonal(Fill(σv^-2,pdim(DM)+1))
     B = ValidationSafetyFactor*σv*sqrt(2*IC);    Ran = range(-B + (ypred-yoffset), B + (ypred-yoffset); length=N)
     GetProfile(DM, pdim(DM)+1, Ran; LogLikelihoodFn=VPL, LogPriorFn=FictDataPointPrior, dof, MLE=mleNew, logLikeMLE=VPL(mleNew), GenerateNewDerivatives=true, 
-                Fisher, Confnum, N, IsCost=true, Domain=nothing, InDomain=nothing, ProfileDomain=nothing, AllowNewMLE=false, general=true, SavePriors=true, kwargs...)
+                Fisher=NewFisher, Confnum, N, IsCost=true, Domain=nothing, InDomain=nothing, ProfileDomain=nothing, AllowNewMLE=false, general=true, SavePriors=true, kwargs...)
 end
 
 # Generate multiple validation profiles and add back offset to prediction scale
@@ -1822,10 +1823,11 @@ Therefore, if the validation profiles are not only computed as an intermediate s
 Otherwise, a small value is chosen by default to make the subsequent prediction profile computation more efficient.
 Most other kwargs are passed on to the `ParameterProfiles` function and thereby also to the optimizers, see e.g. [`ParameterProfiles`](@ref), [`InformationGeometry.minimize`](@ref).
 """
-function ValidationProfiles(DM::AbstractDataModel, yComp::Int, Ts::AbstractVector=range(extrema(xdata(DM))...; length=3length(xdata(DM))); dof::Int=DOF(DM), MLE::AbstractVector{<:Number}=MLE(DM), ModelParExtractor::Function=GetOnlyModelParams(DM),
+function ValidationProfiles(DM::AbstractDataModel, yComp::Int, Ts::AbstractVector=range(extrema(xdata(DM))...; length=3length(xdata(DM))); Confnum::Real=2, dof::Int=DOF(DM), MLE::AbstractVector{<:Number}=MLE(DM), ModelParExtractor::Function=GetOnlyModelParams(DM),
+                        Fisher::AbstractMatrix=FisherMetric(DM, MLE), ScaledInverseFisher::AbstractMatrix=InvChisqCDF(dof, ConfVol(Confnum)) * Symmetric(pinv(Fisher)), VarianceProp::Function=VariancePropagation(DM, MLE, ScaledInverseFisher; Confnum, dof),
                         Model::ModelOrFunction=Predictor(DM), ydim::Int=length(Model(Ts[1], ModelParExtractor(MLE))), IsCost::Bool=true, OffsetToZero::Bool=false, Meta=:ValidationProfiles, parallel::Bool=true, verbose::Bool=true, kwargs...)
     ypreds = @view EmbeddingMap(Val(true), Model, ModelParExtractor(MLE), Ts)[yComp:ydim:end]      # Always compute with offset to zero internally
-    Res = (parallel ? progress_pmap : progress_map)(i->GetValidationProfilePoint(DM, yComp, Ts[i]; ypred=ypreds[i], dof=dof, MLE, Model, ydim=ydim, IsCost, verbose, kwargs...), 1:length(Ts); progress=Progress(length(Ts); enabled=verbose, desc="Computing Validation Profiles... (parallel, $(nworkers()) workers) ", dt=1, showspeed=true))
+    Res = (parallel ? progress_pmap : progress_map)(i->GetValidationProfilePoint(DM, yComp, Ts[i]; ypred=ypreds[i], Confnum, dof, MLE, Model, ydim=ydim, Fisher, ScaledInverseFisher, VarianceProp, IsCost, verbose, kwargs...), 1:length(Ts); progress=Progress(length(Ts); enabled=verbose, desc="Computing Validation Profiles... (parallel, $(nworkers()) workers) ", dt=1, showspeed=true))
     Profs, Trajs = getindex.(Res,1), getindex.(Res,2)
     InsertToZprof(TrajPoint::AbstractVector{T}, i::Int) where T<:Number = T.(Model(Ts[i], ModelParExtractor(@view TrajPoint[1:end-1]))[yComp])
     InsertToZprof(TrajPoint::ComponentVector{T}, i::Int) where T<:Number = T.(Model(Ts[i], ModelParExtractor(TrajPoint.original))[yComp])
